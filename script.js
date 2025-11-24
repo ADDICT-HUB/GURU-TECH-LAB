7ac82eb8d53d39f2de4947240e4aff8b0e00b40e
OFFICIAL GURU <hapawapi0@gmail.com>
2025-11-24 21:51:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Full Guru Tech Marketing Website Loaded');
+// Theme Switcher
+function toggleTheme() {
+  document.body.classList.toggle('light-mode');
+}
+
+// Remove loading screen
+window.onload = () => {
+  document.getElementById('loading-screen').style.display = 'none';
+};
